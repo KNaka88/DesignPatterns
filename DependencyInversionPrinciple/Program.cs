e2ad88e2dde27a61d05bd781cdee1f7801d1dd3b
Koji Nakagawa <lightupthesky0627@gmail.com>
2018-05-16 21:46:01 -0700

    add description

--- DependencyInversionPrinciple/Program.cs
+++ DependencyInversionPrinciple/Program.cs
@@ -2,6 +2,11 @@
 
 namespace DependencyInversionPrinciple
 {
+
+    // Dependency Inversion Principle:
+    // High-level modules should not depend upon low-level modules. Both should depend upon abstractions.
+    // Abstractions should not depend upon details. Details should depend upon abstractions.
+    // https://www.intertech.com/Blog/the-dependency-inversion-principle-with-c-examples/
     class Program
     {
         static void Main(string[] args)
